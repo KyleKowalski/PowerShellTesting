e7c9c41dfa0449b71687ea477a37748e074f1056
Kyle Kowalski <kkowalski@marketo.com>
2018-04-16 11:38:41 -0600

    initial testing, weeee!

---
+++ TestSync.ps1
@@ -0,0 +1,19 @@
+### Initial Testing script for syncing directly from github
+### This will use github credential manager
+
+
+#$cmd = 'git status'
+#$arg1 = 'status'
+#$arg2 = '/f'
+#$arg3 = '$MySourceDirectory\src\Deployment\Installations.xml'
+
+#& $cmd #$arg1 #$arg2 $arg3
+$outputFile = "C:\PowerShellTestingGround\PowerShellLogs\Test-log.txt"
+$commitMessage = "initial testing, weeee!"
+
+git status | out-file $outputFile
+git add .  | out-file $outputFile -append
+git commit -m "$commitMessage" | out-file $outputFile -append
+git status  | out-file $outputFile -append
+git push origin master  | out-file $outputFile -append
+git status  | out-file $outputFile -append
