48015578a9c7ce132842d9220d2c7811374e6810
Kyle Kowalski <kkowalski@marketo.com>
2018-04-18 15:51:23 -0600

    testing inheriting files - initial test looks positive

--- PushLocalRepositoryChangesToServer.ps1
+++ PushLocalRepositoryChangesToServer.ps1
@@ -1,1 +1,13 @@
+$outputFile = "C:\PowerShellTestingGround\PowerShellLogs\UpdateLocalRespository-log.txt"
 
+Write-Output "************* Start" | out-file $outputFile -append
+Write-Output "Starting upload to github... at: $(Get-Date -format 'g')" | out-file $outputFile -append
+
+git status | out-file $outputFile -append
+git add . | out-file $outputFile -append
+git commit -m "$commitMessage" | out-file $outputFile -append
+git status | out-file $outputFile -append
+git push origin master  | out-file $outputFile -append
+git status | out-file $outputFile -append
+
+Write-Output "************* End" | out-file $outputFile -append
---
+++ TestInclude.ps1
@@ -0,0 +1,9 @@
+. "C:\PowerShellTestingGround\PowerShellTesting\TestSync.ps1"
+
+# What I'd like to do here is get a standard set of functions that we include in all of the other files.
+# This is following the idea of DRY coding (Don't Repeat Yourself) 
+# Basically, have code in 1 place if at all possible - and I believe it is possible.
+# We can have a general use (things like color) - AD Common - Database Common - etc, try to simplify individual scripts
+
+
+randomLog("this log called from include - inheriting sync")
