77a758e9aa203c2e2d019d7056365e390a283ea6
Kyle Kowalski <kkowalski@marketo.com>
2018-04-19 13:50:18 -0600

    example server download appears intact

--- PullUpdateFromGithubToLocal.ps1
+++ PullUpdateFromGithubToLocal.ps1
@@ -3,10 +3,16 @@ $outputFile = "C:\PowerShellTestingGround\PowerShellLogs\UpdateLocalRespository-
 Write-Output "************* Start" | out-file $outputFile -append
 Write-Output "Starting download from github... at: $(Get-Date -format 'g')" | out-file $outputFile -append
 
-# this will switch us back to master - we should NEVER navigate off of master on this server
-# this will insure we don't
+# this will nuke any local changes and force it back to a default state 
+# in the event we are on a different branch with changes (should never happen)
+# this will allow us to switch to master under all circumstances (no local changes cached to prevent switch)
+git reset head --hard | out-file $outputFile -append
+
+# this will switch us back to master - we should never navigate off of master on this server
+# and this will insure we don't
 git checkout master
-# this will nuke any local changes and force it back to a default state
+# this will nuke any local changes to master and force it back to a default state
+# again should NOT happen, but now we'll be sure
 git reset head --hard | out-file $outputFile -append
 # this will check the server to insure we're reading the most current information
 git fetch | out-file $outputFile -append
@@ -14,4 +20,5 @@ git fetch | out-file $outputFile -append
 git pull | out-file $outputFile -append
 # this will log us a status which should always be something to the effect of "Your branch is up to date with 'origin/master'."
 git status | out-file $outputFile -append
+
 Write-Output "************* End" | out-file $outputFile -append
