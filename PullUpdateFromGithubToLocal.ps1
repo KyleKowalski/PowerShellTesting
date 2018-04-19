5c92df9e40416d81f02a1683eb2cb5b1fb9a0828
Kyle Kowalski <kkowalski@marketo.com>
2018-04-19 13:35:49 -0600

    minor tweaks to the pull update from github, trying to insure all errors covered

--- PullUpdateFromGithubToLocal.ps1
+++ PullUpdateFromGithubToLocal.ps1
@@ -3,7 +3,15 @@ $outputFile = "C:\PowerShellTestingGround\PowerShellLogs\UpdateLocalRespository-
 Write-Output "************* Start" | out-file $outputFile -append
 Write-Output "Starting download from github... at: $(Get-Date -format 'g')" | out-file $outputFile -append
 
+# this will switch us back to master - we should NEVER navigate off of master on this server
+# this will insure we don't
+git checkout master
+# this will nuke any local changes and force it back to a default state
+git reset head --hard | out-file $outputFile -append
+# this will check the server to insure we're reading the most current information
 git fetch | out-file $outputFile -append
+# this will pull files from the server, looking for any updates
 git pull | out-file $outputFile -append
+# this will log us a status which should always be something to the effect of "Your branch is up to date with 'origin/master'."
 git status | out-file $outputFile -append
 Write-Output "************* End" | out-file $outputFile -append
--- TestInclude.ps1
+++ TestInclude.ps1
@@ -3,7 +3,14 @@
 # What I'd like to do here is get a standard set of functions that we include in all of the other files.
 # This is following the idea of DRY coding (Don't Repeat Yourself) 
 # Basically, have code in 1 place if at all possible - and I believe it is possible.
-# We can have a general use (things like color) - AD Common - Database Common - etc, try to simplify individual scripts
+# We can have a general use (things like color) - Logging - AD Common - Database Common - etc, try to simplify individual scripts
 
+
+# WHY INCLUDE INSTEAD OF POWERSHELL MODULE?
+# I am not sure - I personally like the inclusion method (adding files at the top that you are including)
+# this feels more 'right' in my coding brain.  
+# Modules are ok - and I'm sure they'd work great - they just feel 'hidden' to me.  
+# the above is abundantly clear WHAT you are including, easy to then go and find the target function(s)
+# we can discuss
 
 randomLog("this log called from include - inheriting sync")
