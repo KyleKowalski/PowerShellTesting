2393eaab00fec44c6b8b2f12f20ca399d71df8fc
Kyle Kowalski <kkowalski@marketo.com>
2018-04-25 08:55:06 -0600

    modified script to commit local changes instead of nuking them - roughed out logic

--- PullUpdateFromGithubToLocal.ps1
+++ PullUpdateFromGithubToLocal.ps1
@@ -7,7 +7,24 @@ Write-Output "Starting download from github... at: $(Get-Date -format 'g')" | ou
 # this will nuke any local changes to where we are and force it back to a default state 
 # in the event we are on a different branch with changes (should never happen)
 # this will allow us to switch to master under all circumstances (no local changes cached to prevent switch)
-git reset head --hard | out-file $outputFile -append
+#### git reset head --hard | out-file $outputFile -append # disabled for alternative KK 2018-04-25
+
+# alternatively, it might be better to do an auto commit instead and keep your local branch intact.
+git add .
+git commit -m "auto commit from login script at: $(Get-Date -format 'g')"
+
+# which is better?  keep local changes that may be incomplete or accidental?  Or nuke changes?  
+# TODO:  Build some better logic around this.  
+# Logic...
+#############
+## If on branch master
+#### if no changes go to end
+#### else if changes ?????
+## else if not on master
+### if no changes - switch to master
+### else if changes ????  & switch to master
+# in all cases pull master
+#############
 
 # this will switch us back to master - we should never navigate off of master on this server
 # and this will insure we don't
